c8c7bea17ddbf6ce110ed688e8e80c706bff243c
Noisy <soumyashekhardeb@gmail.com>
2022-01-20 23:59:17 +0530

    3rd push after cloning and pulling

---
+++ src/main/java/UI/pushPull3.java
@@ -0,0 +1,32 @@
+package UI;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class pushPull3 {
+
+	public static String browser = "Chrome";
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+
+		if (browser.equals("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser.equals("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+		driver.get("https://www.sugarcrm.com/au/request-demo/");
+		driver.manage().window().maximize();
+		driver.close();
+
+}
+}
